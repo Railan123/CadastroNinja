d368b44045d2443359f63cedddd8254ac2ea3552
railanpdssantos@gmail.com <railanpdssantos@gmail.com>
2025-02-19 15:10:21 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinja.Missoes;
 
 import dev.java10x.CadastroDeNinja.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor //cria construtor vazio
+@AllArgsConstructor //cria construtor cheio
+@Data // cria todos os Getter e Setter
 public class MissoesModel {
 
     @Id
@@ -18,44 +24,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes") //UMA missão pode ter VÁRIOS (lista) ninjas
     private List<NinjaModel> ninjas;
 
-    public void setNinja(List<NinjaModel> ninja) {
-        this.ninjas = ninja;
-    }
-
-    public List<NinjaModel> getNinja() {
-        return ninjas;
-    }
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(Long id, String nome, String dificuldade) {
-        this.id = id;
-        this.nome = nome;
-        this.dificuldade = dificuldade;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nomeMissao) {
-        this.nome = nomeMissao;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String rank) {
-        this.dificuldade = rank;
-    }
 }
--- src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Ninjas/NinjaModel.java
@@ -1,13 +1,15 @@
 package dev.java10x.CadastroDeNinja.Ninjas;
-
-
 import dev.java10x.CadastroDeNinja.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity //tranforma uma classe em uma ENTIDADE no Banco de Dados
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -18,49 +20,7 @@ public class NinjaModel {
     private int idade;
 
     @ManyToOne //UM ninja tem uma unica missão
-
     @JoinColumn (name = "missoes_id") //FK
-
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
